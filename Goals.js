9d5b809bc46aca0f24aca76ec81a30f5ae37d90e
Eric deRegt <ericderegt@gmail.com>
2018-02-27 08:52:41 -0500

    thoughts

--- Goals.js
+++ Goals.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+
 import { Tile, List, ListItem } from 'react-native-elements';
+import CustomMultiPicker from "react-native-multiple-select-list";
 
 const list = [
   {
@@ -13,20 +15,43 @@ const list = [
   },
 ]
 
+const userList = {
+  "123":"Go to work",
+  "124":"Go to school",
+  "125":"Go out to dinner",
+  "126":"Go to a networking event",
+  "127":"Eat healthy today",
+  "128":"Get 8 hours of sleep",
+  "129":"Cook dinner",
+  "130":"Work out",
+  "131":"Eat sushi",
+
+}
+
 class Goals extends Component {
   render() {
     return (
-      <List>
-        {
-          list.map((item, i) => (
-            <ListItem
-              key={i}
-              title={item.title}
-              leftIcon={{name: item.icon}}
-            />
-          ))
-        }
-      </List>
+      <View style={{flex: 1}}>
+        <View style={{alignItems: 'center', padding: 10}}><Text style={{fontSize:18}}>Set Your Goals</Text></View>
+        <CustomMultiPicker
+          options={userList}
+          search={false} // should show search bar?
+          multiple={true} //
+          placeholder={"Search"}
+          placeholderTextColor={'#757575'}
+          returnValue={"label"} // label or value
+          callback={(res)=>{ console.log(res) }} // callback, array of selected items
+          rowBackgroundColor={"#eee"}
+          rowHeight={40}
+          rowRadius={5}
+          iconColor={"#00a2dd"}
+          iconSize={30}
+          selectedIconName={"ios-checkmark-circle-outline"}
+          unselectedIconName={"ios-radio-button-off-outline"}
+          scrollViewHeight={500}
+          selected={[1,2]} // list of options which are selected by default
+        />
+      </View>
     );
   }
 }
--- Thoughts.js
+++ Thoughts.js
@@ -1,30 +1,60 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
-import { List, ListItem } from 'react-native-elements';
+import {
+  Text,
+  TextInput,
+  Keyboard,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {
+  Card,
+  Button,
+  Icon,
+} from 'react-native-elements';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 
 class Thoughts extends Component {
   render() {
     return (
-      <ScrollView>
-        <List>
-          <ListItem
-            title="Test"
-            rightTitle="test"
-            hideChevron
-          />
-        </List>
-
-        <List>
-          <ListItem
-            title="Birthday"
-            hideChevron
-          />
-          <ListItem
-            title="City"
-            hideChevron
-          />
-        </List>
-      </ScrollView>
+      <View style={styles.container}>
+        <View>
+          <Card
+            title='What are you thinking?'
+            >
+            <TextInput
+              editable={true}
+              backgroundColor='#f5f5f5'
+              style={{height: 200, shadowOffset: {width: 0.25,height: 1}, shadowColor: 'black', shadowOpacity: 0.25}}
+              borderColor='#20b2aa'
+              borderWidth={2}
+              borderRadius={10}
+              multiline={true}
+              placeholder="I ..."
+              clearTextOnFocus={true}
+              onSubmitEditing={Keyboard.dismiss}
+              padding={5}
+              returnKeyType='done'
+            />
+          </Card>
+        </View>
+        <View style={{flexDirection: 'row', padding: 5}}>
+            <View style={{flex: 1}}/>
+            <Button
+              title='Submit'
+              textStyle={{fontSize: 18}}
+              buttonStyle={{width: 100, height: 50, backgroundColor: '#20b2aa'}}
+              iconRight={{ name: 'input'}}
+              raised={true}
+            />
+        </View>
+    </View>
     );
   }
 }
