865c4622a874ea72442a99ce880ff82462b00318
Eric deRegt <ericderegt@gmail.com>
2018-03-04 19:45:20 -0500

    green modal

--- components/home-exercise.js
+++ components/home-exercise.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Card, Button, Icon } from 'react-native-elements';
 
-import MH_Slider from './mh-slider';
 import { GlobalStyles, GlobalColors } from '../themes/global-styles';
+import Modal from "react-native-modal";
 
 const styles = StyleSheet.create({
   container: {
@@ -20,20 +20,42 @@ class ExerciseCard extends Component {
     super(props)
   };
 
+  state = {
+    isModalVisible: false
+  };
+
+  _toggleModal = () =>
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+
   render() {
     return (
-      <Card containerStyle={GlobalStyles.card}>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <View style={{ flex: 1 }}>
-            <Icon name='directions-run' flex={1} size={36} color={GlobalColors.greyColor} />  
+      <View style={{ flex: 1 }}>
+
+        <Card containerStyle={GlobalStyles.card}>
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <View style={{ flex: 1 }}>
+              <Icon name='directions-run' flex={1} size={36} color={GlobalColors.greyColor} />
+            </View>
+            <View style={styles.textBox}>
+              <Text style={GlobalStyles.text}>Exercise</Text>
+              <Text style={GlobalStyles.subText}>Goal: 3 times/week</Text>
+            </View>
+            <TouchableOpacity onPress={this._toggleModal}>
+              <Icon name='add' justifyContent='flexEnd' flex={1} size={36} color={GlobalColors.primaryColor} />
+            </TouchableOpacity>
           </View>
-          <View style={styles.textBox}>
-            <Text style={GlobalStyles.text}>Exercise</Text>
-            <Text style={GlobalStyles.subText}>Goal: 3 times/week</Text>
+        </Card>
+
+        <Modal isVisible={this.state.isModalVisible} backdropOpacity={1.0} backdropColor={GlobalColors.primaryColor}>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Hello!</Text>
+            <TouchableOpacity onPress={this._toggleModal}>
+              <Text>Hide me!</Text>
+            </TouchableOpacity>
           </View>
-          <Icon name='add' justifyContent='flexEnd' flex={1} size={36} color={GlobalColors.primaryColor} />
-        </View>
-      </Card>
+        </Modal>
+
+      </View>
     );
   }
 }
